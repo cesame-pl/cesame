4791462ae7d0ae4b55fae1077d611c134ff107e5
Qian Zhao <qzhao6600@gmail.com>
2024-04-01 16:33:07 -0400

    Fixed Makefile error.

--- src/cesame.ml
+++ src/cesame.ml
@@ -5,7 +5,7 @@
 type action = Ast | Sast (* | LLVM_IR *)
 
 let () =
-  (* let action = ref LLVM_IR in *)
+  let action = ref Ast in 
   let set_action a () = action := a in
   let speclist = [
     ("-a", Arg.Unit (set_action Ast), "Print the AST");
